b0e5990e256375e55296c14acecc82091085a982
Raul Veras <verasraul@gmail.com>
2023-05-08 02:13:41 -0400

    Updating reverseString function

--- level1/reverseString.js
+++ level1/reverseString.js
@@ -3,8 +3,29 @@
  * You're not allowed to use String.prototype.reverse().
  * Returns the new string.
  */
-function reverseString() {
-
+function reverseString(tr) {
+    // Step 1. Create an empty string that will host the new created string
+    let newString = "";
+ 
+    // Step 2. Create the FOR loop
+    /* The starting point of the loop will be (str.length - 1) which corresponds to the 
+       last character of the string, "o"
+       As long as i is greater than or equals 0, the loop will go on
+       We decrement i after each iteration */
+    for (let i = str.length - 1; i >= 0; i--) { 
+        newString += str[i]; // or newString = newString + str[i];
+    }
+    /* Here hello's length equals 5
+        For each iteration: i = str.length - 1 and newString = newString + str[i]
+        First iteration:    i = 5 - 1 = 4,         newString = "" + "o" = "o"
+        Second iteration:   i = 4 - 1 = 3,         newString = "o" + "l" = "ol"
+        Third iteration:    i = 3 - 1 = 2,         newString = "ol" + "l" = "oll"
+        Fourth iteration:   i = 2 - 1 = 1,         newString = "oll" + "e" = "olle"
+        Fifth iteration:    i = 1 - 1 = 0,         newString = "olle" + "h" = "olleh"
+    End of the FOR Loop*/
+ 
+    // Step 3. Return the reversed string
+    return newString; // "olleh"
 }
 
 describe('reverseString', function() {
